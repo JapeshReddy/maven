93089f64f39811f42930905a306a39308230d298
PujarGirish <girishpujar91@gmail.com>
2024-06-06 17:45:36 +0530

    project created

---
+++ src/main/java/updatedatacom/ty/UpdateData.java
@@ -0,0 +1,26 @@
+package updatedatacom.ty;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UpdateData {
+	private static String url="jdbc:postgresql://localhost:5432/shop";
+	private static String user="postgres";
+	private static String password=" ";
+public static void main(String[] args) {
+	try {
+		Class.forName("org.postgresql.Driver");
+		Connection con =DriverManager.getConnection(url, user, password);
+		String sql="UPDATE product set cost=2500 WHERE ID=3";
+		Statement stm=con.createStatement();
+		stm.execute(sql);
+		con.close();
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+}
